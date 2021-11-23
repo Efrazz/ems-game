9c1a3860005569351e6f9561f3089ba1caee621c
melkerydman <melkerrydman@gmail.com>
2021-11-23 10:25:46 +0100

    added variables to game setting, added todo

--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -1,13 +1,25 @@
 import Phaser from "phaser";
 
-import characterImage from "../../public/images/ball.png";
+import playerImage from "../../public/images/ball.png";
 
-let character, keys, world;
+let player, keys, world;
 
 let isMoving = false,
   isJumping = false,
   jumpCount = 0,
-  flip = true;
+  btnDown = false;
+
+// global game options
+let gameOptions = {
+  // platformStartSpeed: 350,
+  // spawnRange: [100, 350],
+  // platformSizeRange: [50, 250],
+  // playerGravity: 900,
+  playerSpeed: 200,
+  jumpForce: 300,
+  playerStartPosition: 200,
+  jumps: 2,
+};
 
 export default class HelloWorldScene extends Phaser.Scene {
   constructor() {
@@ -15,27 +27,24 @@ export default class HelloWorldScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("character", characterImage);
+    this.load.image("player", playerImage);
   }
 
   create() {
-    world = {
-      width: this.physics.world.bounds.width,
-      height: this.physics.world.bounds.height,
-    };
-    character = this.physics.add.sprite(
-      world.width / 2,
-      world.height,
-      "character"
-    );
-    character.setCollideWorldBounds(true);
-
     keys = this.input.keyboard.addKeys({
       up: "w",
       down: "s",
       left: "a",
       right: "d",
     });
+
+    world = {
+      width: this.physics.world.bounds.width,
+      height: this.physics.world.bounds.height,
+    };
+
+    player = this.physics.add.sprite(world.width / 2, world.height, "player");
+    player.setCollideWorldBounds(true);
   }
 
   update() {
@@ -45,39 +54,46 @@ export default class HelloWorldScene extends Phaser.Scene {
       isMoving = true;
     }
 
-    // Character movement
+    // player movement
     if (isMoving || isJumping) {
       if (keys.left.isDown) {
-        character.setVelocityX(-200);
+        player.setVelocityX(-gameOptions.playerSpeed);
       } else if (keys.right.isDown) {
-        character.setVelocityX(200);
+        player.setVelocityX(gameOptions.playerSpeed);
       }
       // Stop horizontal movement
       // else if (keys.down.isDown) {
-      //   character.setVelocityX(0);
+      //   player.setVelocityX(0);
       // }
     } else {
-      character.setVelocityX(0);
+      player.setVelocityX(0);
     }
-    // Character jump
-    if (character.body.blocked.down) {
+    // # player jump
+    // # btnDown is used to only allow on press of button
+    // Reset jump when landing
+    if (player.body.blocked.down) {
       jumpCount = 0;
       isJumping = false;
     }
-    if (keys.up.isDown && flip === true) {
+    if (keys.up.isDown && btnDown === false) {
       jump();
-      flip = false;
+      btnDown = true;
     }
     if (keys.up.isUp) {
-      flip = true;
+      btnDown = false;
     }
   }
 }
 
 function jump() {
   isJumping = true;
-  if (jumpCount < 2) {
-    character.setVelocityY(-300);
+  if (jumpCount < gameOptions.jumps) {
+    player.setVelocityY(-gameOptions.jumpForce);
     jumpCount++;
   }
 }
+
+// What I've done
+// Jump
+// Movement left and right (should probably me changed to world moving for endless runner)
+// Added variables for game settings, same could be done for player settings
